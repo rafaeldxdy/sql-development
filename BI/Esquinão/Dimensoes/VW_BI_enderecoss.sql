0dbd73e49eec4e46b30a6177f8796cd2a076d8d6
Rafael Ribeiro <rmeribeiro@gmail.com>
2025-12-24 11:22:44 -0300

    Sanitizando a branch

--- BI/Esquinão/Dimensoes/VW_BI_enderecoss.sql
+++
@@ -1,5 +0,0 @@
-select CAST(DFrua AS NVARCHAR(3)) + '-' + CAST(DFpredio AS NVARCHAR(3)) + '-' + CAST(DFnivel AS NVARCHAR(3)) + '-' + CAST(DFapto AS NVARCHAR(3)), *
-from TBendereco_armazenagem
-
--- dimensao -> enderecos
--- fato -> 
