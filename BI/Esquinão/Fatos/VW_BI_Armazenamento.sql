5a857653b50d544edd2e051e56a41861985676bf
Rafael Ribeiro <rmeribeiro@gmail.com>
2026-01-01 15:00:45 -0300

    finalizando o projeto de armazenamento

--- BI/Esquinão/Fatos/VW_BI_Armazenamento.sql
+++ BI/Esquinão/Fatos/VW_BI_Armazenamento.sql
@@ -1,86 +1,113 @@
 /*
     AUTOR..................: Rafael Ribeiro
-	AREA...................: Logística
-	MODULO.................: WMS
-	DATA/HORA CRIAÇÂO......: 24/12/2025 15:26 PM
-    DATA/HORA MODIFICAÇÂO..: 31/12/2025 10:57 AM
-	OBJETIVO...............: Relatório analítico de armazenamento para BI.
-
-    Dados:
-
-        (v) Data;
-        (v) ID da ficha;
-        (v) Entidade emitente (fornecedor ou empresa);
-        (v) Qtde. SKUs;
-        (v) Qtde. endereçada;
-        (v) Percentual endereçado;
-        (v) Valor recebido;
-        (v) Custo;
-        (v) Data recebimento;
-        (v) Status endereçamento;
-        (v) Data armazenamento;
-        (v) Usuário endereçamento;
-        (v) Cancelado por;
-        (v) Usuário armazenamento.
-
-    Tabelas base:
-
-        TBficha;
-        TBconferencia;
-        TBitem_conferencia;
-        TBitem_nota_fiscal_entrada;
-        TBnota_fiscal_entrada;
-        TBordem_movimentacao;
-        VWpreco.
+    AREA...................: Logística
+    MODULO.................: WMS
+    DATA/HORA CRIAÇÃO......: 24/12/2025 15:26 PM
+    DATA/HORA MODIFICAÇÃO..: 31/12/2025 11:45 PM
+    DATA/HORA REVISÃO......: 01/01/2026 14:58 PM
+    OBJETIVO...............: Relatório analítico de armazenamento para BI.
 */
 
-IF DBO.OBJECT_ID('VW_BI_Armazenamento') IS NOT NULL 
-    DROP VIEW VW_BI_Armazenamento
+IF OBJECT_ID('VW_BI_Armazenamento') IS NOT NULL 
+    DROP VIEW VW_BI_Armazenamento;
 GO
 
 CREATE VIEW VW_BI_Armazenamento WITH ENCRYPTION
 AS
 
-SELECT DISTINCT
-    TBordem_movimentacao.DFdata_criacao                                                                      AS [Data],
-    TBficha.DFid_ficha                                                                                       AS [ID ficha],
-    ISNULL(TBnota_fiscal_entrada.DFcod_empresa_emitente, TBnota_fiscal_entrada.DFcod_fornecedor_emitente)    AS [Entidade emitente],
-    COUNT(Tbitem_conferencia.DFid_item_nota_fiscal_entrada)                                                  AS [Quantidade SKUs],
-    SUM(CASE WHEN TBordem_movimentacao.DFstatus = 'F' THEN 1 ELSE 0 END)                                     AS [Quantidade endereçada],
-    CAST((
-        SUM(CASE WHEN TBordem_movimentacao.DFstatus = 'F' THEN 1 ELSE 0 END) * 100.0)
-        / NULLIF(COUNT(TBitem_conferencia.DFid_item_nota_fiscal_entrada), 0) AS DECIMAL(5,2))                AS [Percentual endereçado],
-    TBnota_fiscal_entrada.DFvalor_total                                                                      AS [Valor recebido],
-    VWpreco.DFcusto_transferencia                                                                            AS [Custo],
-    FORMAT(TBconferencia.DFdata_emissao, 'dd/MM/yyyy HH:mm:ss')                                              AS [Data recebimento],
-    TBordem_movimentacao.DFstatus                                                                            AS [Status endereçamento],
-    TBordem_movimentacao.DFdata_fim_movto                                                                    AS [Data armazenamento],
-    TBordem_movimentacao.DFid_usuario_criacao                                                                AS [Usuário endereçamento],
-    TBordem_movimentacao.DFid_usuario_cancelado                                                              AS [Cancelado por],
-    TBordem_movimentacao.DFid_usuario_movto                                                                  AS [Usuário armazenamento]
-FROM 
-    TBficha WITH(NOLOCK)
-    JOIN TBconferencia WITH(NOLOCK) ON Tbficha.DFid_ficha = TBconferencia.DFid_ficha
-    JOIN TBitem_conferencia WITH(NOLOCK) ON TBconferencia.DFid_relacao = TBitem_conferencia.DFid_relacao
-    JOIN TBitem_nota_fiscal_entrada WITH(NOLOCK) ON TBitem_nota_fiscal_entrada.DFid_item_nota_fiscal_entrada = TBitem_conferencia.DFid_item_nota_fiscal_entrada
-    JOIN TBnota_fiscal_entrada WITH(NOLOCK) ON TBnota_fiscal_entrada.DFid_nota_fiscal_entrada = TBitem_nota_fiscal_entrada.DFid_nota_fiscal_entrada
-    LEFT JOIN TBordem_movimentacao WITH(NOLOCK) ON TBordem_movimentacao.DFid_item_conferencia = TBitem_conferencia.DFid_item_conferencia
-    JOIN VWpreco WITH(NOLOCK) ON VWpreco.DFcod_item_estoque = TBordem_movimentacao.DFcod_item_estoque
-WHERE 
-    TBconferencia.DFdata_emissao >= '2024-01-01'
-    AND TBordem_movimentacao.DFtipo_movimento = 'Entrada'
-GROUP BY 
-    TBordem_movimentacao.DFdata_criacao,
-    TBficha.DFid_ficha,
-    TBnota_fiscal_entrada.DFcod_empresa_emitente,
-    TBnota_fiscal_entrada.DFcod_fornecedor_emitente, 
-    TBordem_movimentacao.DFstatus,
-    VWpreco.DFcusto_transferencia,
-    TBconferencia.DFdata_emissao,
-    TBordem_movimentacao.DFdata_fim_movto,
-    TBnota_fiscal_entrada.DFvalor_total,
-    TBordem_movimentacao.DFid_usuario_criacao,
-    TBordem_movimentacao.DFid_usuario_cancelado,
-    TBordem_movimentacao.DFid_usuario_movto
-    
+    WITH CTEcusto_menor_unidade_item AS (
+        SELECT
+            VWpreco.DFcod_item_estoque,
+            VWpreco.DFcusto_transferencia,
+            VWpreco.DFdata_ultima_alteracao
+        FROM VWpreco WITH (NOLOCK)
+        JOIN TBunidade_item_estoque WITH (NOLOCK) 
+            ON VWpreco.DFid_unidade_item_estoque = TBunidade_item_estoque.DFid_unidade_item_estoque
+        JOIN (
+            SELECT -- Busca o menor fator para garantir a unidade principal do item
+                TBunidade_item_estoque.DFcod_item_estoque,
+                MIN(TBunidade_item_estoque.DFfator_conversao) AS DFmenor_fator 
+            FROM TBunidade_item_estoque WITH (NOLOCK)
+            WHERE TBunidade_item_estoque.DFativo_inativo = 1
+            GROUP BY TBunidade_item_estoque.DFcod_item_estoque
+        ) AS SBMenor_unidade 
+            ON SBMenor_unidade.DFcod_item_estoque = TBunidade_item_estoque.DFcod_item_estoque 
+            AND SBMenor_unidade.DFmenor_fator = TBunidade_item_estoque.DFfator_conversao
+        JOIN (
+            SELECT -- Busca a data máxima para garantir o custo mais recente
+                DFid_unidade_item_estoque,
+                MAX(DFdata_ultima_alteracao) AS DFmaior_data
+            FROM VWpreco WITH (NOLOCK)
+            GROUP BY DFid_unidade_item_estoque
+        ) AS SBUltima_atualizacao
+            ON VWpreco.DFid_unidade_item_estoque = SBUltima_atualizacao.DFid_unidade_item_estoque
+            AND VWpreco.DFdata_ultima_alteracao = SBUltima_atualizacao.DFmaior_data
+        WHERE 
+            VWpreco.DFcod_empresa = 7
+            AND TBunidade_item_estoque.DFativo_inativo = 1
+    ),
+
+    CTEenderecamento AS (
+        SELECT 
+            TBconferencia.DFdata_emissao AS DFdata_recebimento,
+            TBficha.DFid_ficha,
+            TBficha.DFnum_ficha, -- Só para me ajudar a validar os dados, apagar depois
+            ISNULL(TBnota_fiscal_entrada.DFcod_fornecedor_emitente, TBnota_fiscal_entrada.DFcod_empresa_emitente) AS DFcod_entidade_emitente,
+            TBnota_fiscal_entrada.DFid_nota_fiscal_entrada,
+            TBordem_movimentacao.DFcod_item_estoque,
+            (TBitem_nota_fiscal_entrada.DFqtde * TBunidade_item_estoque.DFfator_conversao) AS DFqtde_nota_fiscal,
+            TBitem_conferencia.DFqtde_definitivo AS DFqtde_recebido,
+            TBconferencia.DFid_usuario_critica_qtde AS DFid_usuario_critica,
+            TBordem_movimentacao.DFdata_criacao,
+            TBordem_movimentacao.DFid_usuario_criacao AS DFid_usuario_enderecamento,
+            (TBordem_movimentacao.DFqtde * TBordem_movimentacao.DFfator_conversao) AS DFqtde_enderecar,
+            TBordem_movimentacao.DFstatus,
+            TBordem_movimentacao.DFdata_inicio_movto AS DFdata_inicio_enderecamento,
+            TBordem_movimentacao.DFdata_fim_movto AS DFdata_fim_enderecamento,
+            TBordem_movimentacao.DFid_usuario_movto AS DFid_usuario_armazenamento,
+            TBordem_movimentacao.DFid_usuario_cancelado AS DFid_usuario_cancelamento
+        FROM 
+            TBordem_movimentacao WITH (NOLOCK)
+            JOIN TBitem_conferencia WITH (NOLOCK) 
+                ON TBitem_conferencia.DFid_item_conferencia = TBordem_movimentacao.DFid_item_conferencia
+            JOIN TBitem_nota_fiscal_entrada WITH (NOLOCK)
+                ON TBitem_nota_fiscal_entrada.DFid_item_nota_fiscal_entrada = TBitem_conferencia.DFid_item_nota_fiscal_entrada
+            JOIN TBnota_fiscal_entrada WITH (NOLOCK)
+                ON TBnota_fiscal_entrada.DFid_nota_fiscal_entrada = TBitem_nota_fiscal_entrada.DFid_nota_fiscal_entrada
+            JOIN TBunidade_item_estoque WITH (NOLOCK)
+                ON TBunidade_item_estoque.DFid_unidade_item_estoque = TBitem_nota_fiscal_entrada.DFid_unidade_item_estoque
+            JOIN TBconferencia WITH (NOLOCK)
+                ON TBconferencia.DFid_relacao = TBitem_conferencia.DFid_relacao
+            LEFT JOIN TBficha WITH (NOLOCK)
+                ON TBficha.DFid_ficha = TBconferencia.DFid_ficha
+        WHERE 
+            DFtipo_movimento = 'Entrada'
+            AND TBordem_movimentacao.DFdata_criacao >= '2024-01-01'
+    )
+
+    SELECT 
+        CTEenderecamento.DFdata_recebimento,
+        CTEenderecamento.DFid_ficha,
+        CTEenderecamento.DFnum_ficha, -- Só para me ajudar a validar os dados, apagar depois
+        CTEenderecamento.DFcod_entidade_emitente,
+        CTEenderecamento.DFid_nota_fiscal_entrada,
+        CTEenderecamento.DFcod_item_estoque,
+        CTEenderecamento.DFqtde_nota_fiscal,
+        CTEenderecamento.DFqtde_recebido,
+        CTEcusto_menor_unidade_item.DFcusto_transferencia,
+        CTEenderecamento.DFid_usuario_critica,
+        CTEenderecamento.DFdata_criacao,
+        CTEenderecamento.DFid_usuario_enderecamento,
+        CTEenderecamento.DFqtde_enderecar,
+        CTEenderecamento.DFstatus,
+        CTEenderecamento.DFdata_inicio_enderecamento,
+        CTEenderecamento.DFdata_fim_enderecamento,
+        CTEenderecamento.DFid_usuario_armazenamento,
+        CTEenderecamento.DFid_usuario_cancelamento
+    FROM 
+        CTEenderecamento
+    JOIN 
+        CTEcusto_menor_unidade_item
+            ON CTEenderecamento.DFcod_item_estoque = CTEcusto_menor_unidade_item.DFcod_item_estoque
+
 GO
